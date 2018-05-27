4c51d8ed97faa3cc45e0e135668e4ff8c60671a0
SoXeR <SoXeR@SoXeR-PC.home>
2018-05-27 19:39:35 +0200

    starting with UI, added basic Pane methods, no structur

--- src/buildings/BuildingsPresentation.java
+++ src/buildings/BuildingsPresentation.java
@@ -9,10 +9,15 @@ import java.io.File;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Orientation;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SplitPane;
+import javafx.scene.control.ToolBar;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
@@ -25,7 +30,7 @@ import javafx.stage.Stage;
  * @version 0.1
  */
 public class BuildingsPresentation extends Application {
-    
+        
      /**
      * @param args the command line arguments
      */
@@ -35,26 +40,53 @@ public class BuildingsPresentation extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction((ActionEvent event) -> {
-            /*BuildingsData.openFile(primaryStage);
-            FileChooser fileChooser = new FileChooser();
-            fileChooser.setTitle("Open Resource File");
-            File file = fileChooser.showOpenDialog(primaryStage);
-            BuildingsData.readFile(file.getPath);
-            */
-            System.out.println("Hello World!");
-        });
-        
-        HBox root = new HBox();
-        root.getChildren().add(btn);
+        VBox root = new VBox();
+        //root.getChildren().add(toolBar(), splitRoot());
         
         Scene scene = new Scene(root, 300, 250);
         
-        primaryStage.setTitle("Hello World!");
+        primaryStage.setTitle("Buildings");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
  
+    public ToolBar toolBar() {
+        // add Buttons to Toolbar for
+        // Open File, Save, add Entry, delete entry, undoi, redo, (mapView), searchBox (far right side)
+        ToolBar toolBar = new ToolBar();
+        
+        return toolBar;
+    }
+    
+    public SplitPane splitRoot() {
+        SplitPane splitRoot = new SplitPane();
+        splitRoot.setOrientation(Orientation.HORIZONTAL);
+        return splitRoot;
+    }
+    
+    public ListView listView() {
+        ListView leftView = new ListView();
+        return leftView;
+    }
+    
+    public VBox detailBox() {
+        VBox detailBox = new VBox();
+        return detailBox;
+    }
+    
+    public SplitPane splitDetailBoxTop() {
+        SplitPane splitDetailBoxTop = new SplitPane();
+        splitDetailBoxTop.setOrientation(Orientation.HORIZONTAL);
+        return splitDetailBoxTop;
+    }
+    
+    public void detailViewBottom() {
+        // Should display certain attributes in separated fields at the bottom of the right side VBOX
+    }
 }
+    /*
+    * General structur: VBOX containing Toolbar and Splitpane1 (horizontal)
+    * Splitpane1 containing Listview (left) and VBOX (right)
+    * VBox containing Splitpane2 (horizontal)(top) and Detailview bottom
+    * Splitpane2 containing Main attributes (left) and ImageView(Right)
+    */
