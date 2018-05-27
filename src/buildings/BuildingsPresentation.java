1b2ef878198dda702b4c8b51f9c130e3f0b9a8b7
SoXeR <SoXeR@SoXeR-PC.home>
2018-05-27 23:05:21 +0200

    addded AnchorPanes

--- src/buildings/BuildingsPresentation.java
+++ src/buildings/BuildingsPresentation.java
@@ -15,6 +15,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SplitPane;
 import javafx.scene.control.ToolBar;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
@@ -40,16 +41,50 @@ public class BuildingsPresentation extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        VBox root = new VBox(toolBar(), splitRoot());
-        root.setPrefWidth(600);
-        root.setPrefHeight(400);
+        AnchorPane anchorRoot = new AnchorPane();
+        primaryStage.setMinWidth(300);
+        primaryStage.setMinHeight(200);
         
-        Scene scene = new Scene(root, 400, 600);
+        anchorRoot.setPrefWidth(600);
+        anchorRoot.setPrefHeight(400);
+       
+        
+        VBox root = root();
+        AnchorPane.setTopAnchor(root,1.0);
+        AnchorPane.setLeftAnchor(root,1.0);
+        AnchorPane.setBottomAnchor(root,1.0);
+        AnchorPane.setRightAnchor(root, 1.0);
+        anchorRoot.getChildren().add(root);
+        
+        Scene scene = new Scene(anchorRoot, 600, 400);
         
         primaryStage.setTitle("Buildings");
         primaryStage.setScene(scene);
         scene.getStylesheets().add(BuildingsPresentation.class.getResource("/ressources/BuildingsWithStyle.css").toExternalForm());
         primaryStage.show();
+    }
+    
+    
+    public VBox root() {
+        VBox root = new VBox(toolBar(), rootBottomAnchor());
+        return root;
+    }
+    
+    
+    
+    public AnchorPane rootBottomAnchor() {
+        AnchorPane anchorRootBottom = new AnchorPane();
+        SplitPane splitRootBottom = splitRoot();
+        
+        AnchorPane.setTopAnchor(splitRootBottom,0.0);
+        AnchorPane.setLeftAnchor(splitRootBottom,0.0);
+        AnchorPane.setBottomAnchor(splitRootBottom,0.0);
+        AnchorPane.setRightAnchor(splitRootBottom, 0.0);
+        anchorRootBottom.getChildren().add(splitRootBottom);
+        
+        splitRootBottom.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        
+        return anchorRootBottom;
     }
  
     public ToolBar toolBar() {
@@ -80,10 +115,36 @@ public class BuildingsPresentation extends Application {
     }
     
     public SplitPane splitRoot() {
-        SplitPane splitRoot = new SplitPane();
+        SplitPane splitRoot = new SplitPane(anchorRootLeft(), anchorRootRight());
         splitRoot.setOrientation(Orientation.HORIZONTAL);
         splitRoot.setDividerPositions(0.3);
         return splitRoot;
+    }
+    
+    public AnchorPane anchorRootLeft() {
+        AnchorPane anchorRootLeft = new AnchorPane();
+        ListView leftView = listView();
+        
+        AnchorPane.setTopAnchor(leftView,0.0);
+        AnchorPane.setLeftAnchor(leftView,0.0);
+        AnchorPane.setBottomAnchor(leftView,0.0);
+        AnchorPane.setRightAnchor(leftView, 0.0);
+        anchorRootLeft.getChildren().add(leftView);
+        
+        return anchorRootLeft;
+    }
+    
+    public AnchorPane anchorRootRight() {
+        AnchorPane anchorRootRight = new AnchorPane();
+        VBox detailBox = detailBox();
+        
+        AnchorPane.setTopAnchor(detailBox,0.0);
+        AnchorPane.setLeftAnchor(detailBox,0.0);
+        AnchorPane.setBottomAnchor(detailBox,0.0);
+        AnchorPane.setRightAnchor(detailBox, 0.0);
+        anchorRootRight.getChildren().add(detailBox);
+        
+        return anchorRootRight;  
     }
     
     public ListView listView() {
@@ -92,17 +153,44 @@ public class BuildingsPresentation extends Application {
     }
     
     public VBox detailBox() {
-        VBox detailBox = new VBox();
+        VBox detailBox = new VBox(splitDetailBoxTop(),detailViewBottom());
+        
         return detailBox;
     }
     
+    public AnchorPane anchorDetailBoxTopLeft() {
+        AnchorPane anchorDetailBoxTopLeft = new AnchorPane();
+        
+        Button btn = new Button();
+        btn.setText("button");
+        anchorDetailBoxTopLeft.getChildren().add(btn);
+        
+        return anchorDetailBoxTopLeft;
+    }
+    
+    public AnchorPane anchorDetailBoxTopRight() {
+        AnchorPane anchorDetailBoxTopRight = new AnchorPane();
+        
+        Button btn = new Button();
+        btn.setText("button");
+        anchorDetailBoxTopRight.getChildren().add(btn);
+        
+        return anchorDetailBoxTopRight;
+    }
+    
+    
     public SplitPane splitDetailBoxTop() {
-        SplitPane splitDetailBoxTop = new SplitPane();
+        SplitPane splitDetailBoxTop = new SplitPane(anchorDetailBoxTopLeft(), anchorDetailBoxTopRight());
         splitDetailBoxTop.setOrientation(Orientation.HORIZONTAL);
+        splitDetailBoxTop.setPrefWidth(200);
+        splitDetailBoxTop.setPrefHeight(160);
+        splitDetailBoxTop.setDividerPositions(0.65);
+        
         return splitDetailBoxTop;
     }
     
-    public void detailViewBottom() {
+    public VBox detailViewBottom() {
         // Should display certain attributes in separated fields at the bottom of the right side VBOX
+        return new VBox();
     }
 }
