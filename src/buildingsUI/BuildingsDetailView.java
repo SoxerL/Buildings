f80285248ce3c7f013160b5e8e4897a843fdac71
Sacha Leemann <sacha.leemann@students.fhnw.ch>
2018-05-28 14:43:49 +0200

    added detailView class

---
+++ src/buildingsUI/BuildingsDetailView.java
@@ -0,0 +1,16 @@
+/*
+ * OOP2 Project - Buildings view application
+ * Author: Sacha Leemann
+ * This class is part of the presentation model for this Project
+ * and contains the detailed View of an Objekt on the right Side
+ * of the application
+ */
+package buildingsUI;
+
+/**
+ *
+ * @author Sacha
+ */
+public class BuildingsDetailView {
+    
+}
